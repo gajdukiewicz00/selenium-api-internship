094a9cbbd04c630c7a66eae12e69a6003115073c
gajdukiewicz00 <den.94dew@gmail.com>
2025-04-10 16:41:02 +0200

    update

--- src/test/java/tests/demoQA/exceptionTests/ExceptionTests.java
+++ src/test/java/tests/demoQA/exceptionTests/ExceptionTests.java
@@ -4,6 +4,7 @@ package tests.demoQA.exceptionTests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.testng.annotations.Test;
+import tests.BaseTest;
 
 import static org.testng.Assert.*;
 
@@ -12,18 +13,15 @@ public class ExceptionTests extends BaseTest {
     @Test
     public void testElementNotFoundException() {
         pages.exceptionPage().open();
-        try {
-            pages.exceptionPage().findMissingElementById("fakeId");
-            fail("Expected NoSuchElementException");
-        } catch (NoSuchElementException e) {
-            System.out.println("Element not found, as expected.");
-        }
+
+        pages.exceptionPage().assertElementVisible();
     }
 
     @Test
     public void testSafeElementCheck() {
         pages.exceptionPage().open();
-        assertFalse(pages.exceptionPage().isElementPresent("definitely-fake"));
+
+        pages.exceptionPage().assertElementVisible(pages.exceptionPage().assertElementVisible());
     }
 }
 
